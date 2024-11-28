3a49b25fbedbd3ae4a3563753f7556115ed72996
DionisioArancibia <dionisioarancibiaisla@gmail.com>
2024-11-28 01:24:07 -0400

    implementacion del Template Bootstrap

--- Hamburgueseria/WebHamburgueseria/obj/Debug/net8.0/WebHamburgueseria.AssemblyInfo.cs
+++ Hamburgueseria/WebHamburgueseria/obj/Debug/net8.0/WebHamburgueseria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebHamburgueseria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23dc87218d51a9c45b18cbdd5cc1307acf65671b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cda16aa6e32e8e58dcaa3aba24dcc2d5eba3a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebHamburgueseria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebHamburgueseria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
